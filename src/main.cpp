b810df43c0392a3c63aca6c1be5c89b0dde1c328
Stefan Stojanovic <eastapolo@hotmail.com>
2019-05-19 21:13:22 -0500

    Add start script and update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
+/* Boilerplate code for i3ipcpp lib and to start bar */
 #include <iostream>
-#include <i3ipc++/ipc.hpp>
+#include "../i3ipc++/include/i3ipc++/ipc.hpp"
+
+const std::string left = "%{l}";
 
 int main(){
     i3ipc::connection  conn;
     conn.subscribe(i3ipc::ET_WORKSPACE | i3ipc::ET_BINDING);
+
+    conn.signal_workspace_event.connect([](const i3ipc::workspace_event_t&  ev){
+        if(ev.current->num == 1){
+            std::cout << left << ev.current->num << std::endl; 
+        }
+        if(ev.current->num > 1){
+            std::cout << left << ev.old->num << '\t' << ev.current->num << std::endl;
+        }
+    });
+
+    while (true) {
+        conn.handle_event();
+            
+    }
+
 }
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+bin/apolo-bar | lemonbar -b -f "Noto Sans" -B#1A1A1A -F#DADADA
