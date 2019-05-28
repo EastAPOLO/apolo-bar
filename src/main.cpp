816931accd0d332e964a3d19e79c54a69ce09555
Stefan Stojanovic <eastapolo@hotmail.com>
2019-05-28 13:24:13 -0500

    Initial support for workspaces

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,43 @@
 #include <iostream>
 #include "../i3ipc++/include/i3ipc++/ipc.hpp"
 
+/* Bar settings */
 const std::string left = "%{l}";
+const std::string right = "%{r}";
+const std::string focused = "%{B#383838}";
+const std::string urgent = "%{B#992525}";
+const std::string underline = "%{B#3A85E2}";
+const std::string button = "${B#2B2B2B}";
+
+/* Bar content */
+std::string workspaces = "";
+
+void get_workspaces(i3ipc::connection& conn, std::string& workspace){
+    auto ws = conn.get_workspaces();
+
+    int count = 0;
+    for(int x = 1; x < 11; ++x){
+        if(ws.size() > count && ws[count]->num == x){
+            if(ws[count]->focused){
+                workspace += focused + std::to_string(ws[count]->num);
+            }
+            if(ws[count]->urgent){
+                workspace += urgent + std::to_string(ws[count]->num);
+            }
+            ++count;
+        }
+    }
+    
+}
 
 int main(){
     i3ipc::connection  conn;
-    conn.subscribe(i3ipc::ET_WORKSPACE | i3ipc::ET_BINDING);
+    conn.subscribe(i3ipc::ET_WORKSPACE | i3ipc::ET_WINDOW | i3ipc::ET_MODE | i3ipc::ET_BINDING);
+    get_workspaces(conn, workspaces);
+    std::cout << workspaces << std::endl;
 
-    conn.signal_workspace_event.connect([](const i3ipc::workspace_event_t&  ev){
-        if(ev.current->num == 1){
-            std::cout << left << ev.current->num << std::endl; 
-        }
-        if(ev.current->num > 1){
-            std::cout << left << ev.old->num << '\t' << ev.current->num << std::endl;
-        }
-    });
-
-    while (true) {
+    while(true){
         conn.handle_event();
-            
     }
 
 }
