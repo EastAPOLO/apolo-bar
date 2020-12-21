11a3fabcacb1bb9ec109cbd258c77903ef2e4c56
Stefan Stojanovic <eastapolo@hotmail.com>
2020-12-21 15:07:03 -0600

    Make unnecessary global variables local

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,8 @@ const std::string urgent = "%{B#992525}";
 const std::string button = "%{B#383838}";
 const std::string underline = "%{+u}%{O1920}";
 
-/* Bar content */
-std::string workspaces = "";
-std::string local_clock = "";
-
-void init_bar(){
-    std::cout << underline << left << workspaces << right << local_clock << std::endl;
+void init_bar(std::string &workspaces, std::string &clock){
+    std::cout << underline << left << workspaces << right << clock << std::endl;
 }
 
 void get_workspaces(i3ipc::connection& conn, std::string& workspace){
@@ -39,12 +35,12 @@ void get_workspaces(i3ipc::connection& conn, std::string& workspace){
                 else{
                     workspace += "%{O8}" + std::to_string(ws[x]->num) + "%{O8}" + "%{B-}%{O4}";
                 }
-            } 
+            }
         }
     }
 }
 
-void get_time(std::string& l_c){
+void get_time(std::string& l_c, std::string &workspaces){
     while(true){
         l_c = "";
         std::time_t t = time(nullptr);
@@ -60,28 +56,32 @@ void get_time(std::string& l_c){
         }else {
             l_c += std::to_string(current->tm_min) + "%{O8}" + "%{B-}";
         }
-        init_bar();
+        init_bar(workspaces, l_c);
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
 
-void workspace_event(const i3ipc::workspace_event_t& ev, i3ipc::connection& conn){
+void workspace_event(const i3ipc::workspace_event_t& ev, i3ipc::connection& conn, std::string &workspaces, std::string &clock){
     get_workspaces(conn, workspaces);
-    init_bar();
+    init_bar(workspaces, clock);
 }
 
 int main(){
-    std::thread t1(get_time, std::ref(local_clock));
-    i3ipc::connection conn; 
+    /* Bar Content */
+    std::string workspaces;
+    std::string clock;
+
+    std::thread t1(get_time, std::ref(clock), std::ref(workspaces));
+    i3ipc::connection conn;
     conn.subscribe(i3ipc::ET_WORKSPACE | i3ipc::ET_WINDOW | i3ipc::ET_MODE | i3ipc::ET_BINDING);
-    conn.signal_workspace_event.connect(sigc::bind<i3ipc::connection&>(&workspace_event, conn));
+    conn.signal_workspace_event.connect(sigc::bind<i3ipc::connection&>(&workspace_event, conn, workspaces, clock));
     get_workspaces(conn, workspaces);
-    init_bar();
+    init_bar(workspaces, clock);
 
     while(true){
         conn.handle_event();
-    } 
+    }
 
     t1.join();
 
